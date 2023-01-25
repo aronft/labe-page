52d456dfa5dfed8dcb6c388c671710a5abc0389c
aron <blazte.aster@gmail.com>
2023-01-25 17:48:46 -0500

    add theme screen media queries

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {}
+    screen: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    }
   },
   plugins: []
 }
