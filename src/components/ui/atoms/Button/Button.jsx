62e06f54c2892e0420998aebf029db32a0ba8337
aron <blazte.aster@gmail.com>
2023-01-26 15:16:13 -0500

    create button component

---
+++ src/components/ui/atoms/Button/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Button = ({ children }) => {
+  return (
+    <button className='bg-green text-white rounded-3xl p-3 px-7 text-lg'>{children}</button>
+  )
+}
