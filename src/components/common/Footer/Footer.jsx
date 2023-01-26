8b0d4dbd4292b563e377917d81e3852ed42b0ff4
aron <blazte.aster@gmail.com>
2023-01-26 15:15:07 -0500

    add container component to footer

---
+++ src/components/common/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from '../../ui/object/Container/Container'
+
+export const Footer = () => {
+  return (
+    <div>
+      <Container>Footer</Container>
+    </div>
+  )
+}
