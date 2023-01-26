04b2d8b4f6d738d5ff60f4aac85908fda7be81d4
aron <blazte.aster@gmail.com>
2023-01-26 15:14:55 -0500

    involve Tour component in main tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
+import { Footer } from './components/common/Footer/Footer'
+import { Header } from './components/common/Header/Header'
 import { Tour } from './pages/Tour/Tour'
 
 function App () {
   return (
     <div className=''>
       <Header />
-      <Tour />
+      <main>
+        <Tour />
+      </main>
       <Footer />
     </div>
   )
