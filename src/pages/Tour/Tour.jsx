e5f5f42a008db351b9172ff191a2966b77ec8d3e
aron <blazte.aster@gmail.com>
2023-01-26 15:17:47 -0500

    Create Tour Page component with Container compnoent

--- src/pages/Tour/Tour.jsx
+++ src/pages/Tour/Tour.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Container } from '../../components/ui/object/Container/Container'
 
 export const Tour = () => {
   return (
-    <div>Tour</div>
+    <div>
+      <Container>
+        <h1 className='text-4xl md:text-7xl font-bold font-serif'><span className='text-black'>Lobe</span> <span className='text-green'>Tour</span></h1>
+      </Container>
+    </div>
   )
 }
